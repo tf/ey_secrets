02626d13522429e7cc61e9b9463e8fb229f5c8e3
Tim Fischbach <tfischbach@codevise.de>
2014-08-20 13:15:44 +0200

    fix dry-run

--- lib/ey_secrets/adapters/shell.rb
+++ lib/ey_secrets/adapters/shell.rb
@@ -8,10 +8,11 @@ module EySecrets
     def self.execute!(action, options = {})
       action.commands.each do |command|
         yield(command) if block_given?
-        return if options[:dry_run]
 
-        unless system(command)
-          raise(CommandFailed, "Error while executing #{command}")
+        unless options[:dry_run]
+          unless system(command)
+            raise(CommandFailed, "Error while executing #{command}")
+          end
         end
       end
     end

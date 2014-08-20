6197cd7e032b431db55bfaf948e7f14c46623a73
Tim Fischbach <tfischbach@codevise.de>
2014-08-20 12:55:30 +0200

    turn dry-run into boolean parameter

--- lib/ey_secrets/adapters/cli.rb
+++ lib/ey_secrets/adapters/cli.rb
@@ -5,7 +5,7 @@ module EySecrets
   class Cli < Thor
     method_option 'environment', aliases: '-e', type: :string, required: true, banner: 'Environment to update.'
     method_option 'app', aliases: '-a', type: :string, banner: 'App to update.'
-    method_option 'dry-run', aliases: '-n', type: :string, banner: 'Only print commands do not execute.'
+    method_option 'dry-run', aliases: '-n', type: :boolean, banner: 'Only print commands do not execute.'
     desc 'update', 'Copy config files from deploy/config to instances.'
     def update
       Shell.execute!(Update.new(EngineYard.applications, Git.repository('.'), options), options) do |command|

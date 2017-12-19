536b2df6a9c8f341f41747fe1db908fda361636d
Tim Fischbach <tfischbach@codevise.de>
2017-12-19 13:14:15 +0100

    Improve repository not ready error message
    Explicitly mention that config can only be updated from master branch.

--- lib/ey_secrets/models/repository.rb
+++ lib/ey_secrets/models/repository.rb
@@ -13,7 +13,12 @@ module EySecrets
 
     def assert_ready_for_update
       unless ready_for_update?
-        raise(NotReadyForUpdate, "Please, commit and push your changes before updating config.")
+        raise(NotReadyForUpdate,
+              'Commit on master and push your changes before updating config.' \
+              "\n\n" \
+              'Config can only be updated from the master branch. ' \
+              'This helps ensure that the credentials found on a production ' \
+              'system always reflect the current state of the master branch.')
       end
     end
   end
